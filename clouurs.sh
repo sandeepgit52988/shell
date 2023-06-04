b08f33b2849ce9b775ab735e959d9f9011818d07
sandeepgit52988 <needawsanddevops@gmail.com>
2023-06-04 14:09:06 +0530

    Auto push

--- clouurs.sh
+++ clouurs.sh
@@ -1,7 +1,7 @@
-Code	Color	Example
-30	Black	echo -e "Default \e[30mBlack"
-31	Red	echo -e "Default \e[31mRed"
-32	Green	echo -e "Default \e[32mGreen"
-33	Yellow	echo -e "Default \e[33mYellow"
+#Code	Color	Example
+#30	Black	echo -e "Default \e[30mBlack"
+#31	Red	echo -e "Default \e[31mRed"
+#32	Green	echo -e "Default \e[32mGreen"
+#33	Yellow	echo -e "Default \e[33mYellow"
 
 echo -e "\e[32m This is Green colour \e[0m"
